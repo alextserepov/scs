6f7db6ba5f75d7f3f95186bdeca714b563969bc4
Aleksandr Tserepov-Savolainen <alextserepov@gmail.com>
2022-07-11 08:15:51 +0300

    Convert NIX Derivation to BoM
    Signed-off-by: Aleksandr Tserepov-Savolainen <alextserepov@gmail.com>

---
+++ python/derivation.py
@@ -0,0 +1,17 @@
+import json
+import re
+
+f=open('prov.json')
+data = json.load(f)
+for i in data:
+    pre=re.sub('/nix/store/[0-9a-z]*-', '', i)
+    post=re.sub('.drv', '', pre)
+    post=re.sub('.tar.*', '', post)
+    post=re.sub('.zip', '', post)
+    post=re.sub('.tgz', '', post)
+    if(re.search(r'\d', post) and not re.search('CVE', post, re.IGNORECASE) and not re.search('patch', post, re.IGNORECASE) and not re.search('\?', post) and not re.search('\.diff', post)):
+        app = re.sub('-[0-9].*', '', post)
+        ver = re.sub('.*-', '', post)
+        if (len(app)>0 and len(ver)>0):
+            print('{"app": "'+app+'", "version": "' + ver + '"}')
+f.close()
